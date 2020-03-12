91f46075d1ff2fc1c374b121873d4e901a226025
Md.Sumon Sarder <sumonsorder953@gmail.com>
2020-03-12 12:10:29 +0600

    custome text

---
+++ app/src/main/java/com/example/custormfont/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.custormfont;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
