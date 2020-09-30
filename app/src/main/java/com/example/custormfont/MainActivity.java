aa3477c087673ce3f8db86e57a4b397a8dd04501
Devloper-Adil <62999965+Devloper-Adil@users.noreply.github.com>
2020-10-01 01:56:10 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/custormfont/MainActivity.java
+++ app/src/main/java/com/example/custormfont/MainActivity.java
@@ -15,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //In AndroidStudio 4.0.1 You can Directly Change The Font,
+        //No Need To Code.
+        //Thank You
 
 
     }
